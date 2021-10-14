92973a5df96fadf2dcff150be4fa44011cf45428
Marina Monsivais <minamonsi@me.com>
2021-10-13 21:41:46 -0500

    WIP

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,8 +1,25 @@
 //importing accounts-model giving us access to the functions and db calls in the accounts-model
 const Account = require("./accounts-model")
+
 exports.checkAccountPayload = (req, res, next) => {
-  console.log("check middleware")
-  next()
+  const error = { status : 400 }
+  const {name, budget} = req.body
+  if(name === undefined || budget === undefined){
+    error.message = 'name and budget are required'
+  }else if(typeof name !== 'string') {
+    error.message = 'name of account must be a string'
+  }else if(name.trim().length < 3 || name.trim().length > 100){
+    error.message = 'name of account must be between 3 and 100'
+  }else if( typeof budget !== 'number' || isNaN(budget)) {
+    error.message = 'budget of account must be a number'
+  }else if(budget < 0 || budget > 1000000){
+    error.message = 'budget of account is too large or too small'
+  }
+  if(error.message){
+    next(error)
+  }else{
+    next()
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
@@ -10,7 +27,7 @@ exports.checkAccountNameUnique = (req, res, next) => {
   next()
 }
 
-exports.checkAccountId = async (req, res, next) => {//checkAccountId checks the accounts db 
+exports.checkAccountId = async (req, res, next) => {//checkAccountId mw will check the accounts db 
   try{
     const account = await Account.getById(req.params.id)
     if(!account){
